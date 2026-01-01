b22de79667a190f776e9a1ac1d2e9fb6cfc84f34
krati-gg <65992828+krati-gg@users.noreply.github.com>
2026-01-01 14:43:33 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,9 @@
+
+
+num1= int(input("Enter First Number: "))
+num2= int(input("Enter Second Number: "))
+print("Addition: "  + str(num1+num2))
+print("Subtraction: "  + str(num1-num2))
+print("Multiplication: "  + str(num1*num2))
+print("Division: "  + str(num1/num2))
+
---
+++ Task2.py
@@ -0,0 +1,4 @@
+
+FirstName = str(input('Enter First Name: '))
+LastName= str(input('Enter Last Name: '))
+print("Hello " + FirstName + " " + LastName + "! Welcome to Python program")
